2bfe75694c83f7e9442e83281cb9e4c1295c31fd
khushifulara2002 <106725724+khushifulara2002@users.noreply.github.com>
2022-06-02 11:49:59 +0530

    Create 17.py

---
+++ 17.py
@@ -0,0 +1,9 @@
+#find  no. is positive or negative
+
+a=int(input("enter any number ="))
+
+if(a>=0):
+    print("{0} is positive number".format(a))
+
+else:
+    print("{0} is a negative number".format(a))
