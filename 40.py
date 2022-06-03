1dd608275786fcb3780908763d8dc1a286b1a780
khushifulara2002 <106725724+khushifulara2002@users.noreply.github.com>
2022-06-03 16:34:33 +0530

    Update 40.py

--- 40.py
+++ 40.py
@@ -1,9 +1,11 @@
-#sum of 10 numbers and skip negative numbers
+#find sum of 10 no. until user enter positive no.
+
 
 a=10
 sum=0
 for i in range(1,a+1):
     num=int(input("enter %d number ="%i))
     if(num>0):
-        sum=num+sum
+        break
+    sum=num+sum
 print("sum",sum)
