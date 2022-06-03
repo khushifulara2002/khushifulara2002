d377c6d5d118e84a705039d1736bcd9922c5518d
khushifulara2002 <106725724+khushifulara2002@users.noreply.github.com>
2022-06-03 16:43:54 +0530

    Create 49.py

---
+++ 49.py
@@ -0,0 +1,10 @@
+#find sum of odd no.
+
+sum =0
+a=int(input("enter upper limit ="))
+
+print("the sum of odd number between 1 and %d = "%a)
+for i in range(1,a+1):
+    if(i%2!=0):
+       sum=sum+i
+print(sum)
