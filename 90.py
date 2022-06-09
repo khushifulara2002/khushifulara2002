0801ce11bd8c273120de9ec30bc3281dfd44cc5d
khushifulara2002 <106725724+khushifulara2002@users.noreply.github.com>
2022-06-09 23:09:09 +0530

    Create 90.py

---
+++ 90.py
@@ -0,0 +1,8 @@
+#vowel or constant
+
+ab=input(" enter any albhabets=")
+
+if(ab=='a'or ab=='e'or ab=='i'or ab=='o'or ab=='u' or ab=='A' or ab=='E' or ab=='I' or ab=='O' or ab=='U'):
+    print("%c is a vowel"%ab)
+else:
+    print("%c is a constant"%ab)
