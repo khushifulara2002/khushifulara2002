7553c98e0fc01b55581c687f2196aa6862b63530
khushifulara2002 <106725724+khushifulara2002@users.noreply.github.com>
2022-06-09 23:08:16 +0530

    Create 88.py

---
+++ 88.py
@@ -0,0 +1,10 @@
+#lower case or upper case
+
+ab=input("please enter any character =")
+if(ab>='a'and ab<='z'):
+    print("%c is a lowercase"%ab)
+if(ab>='A' and ab<='Z'):
+    print("%c is a uppercase"%ab)
+else:
+    print("%c is not a lowercase nor a uppercase"%ab)
+
