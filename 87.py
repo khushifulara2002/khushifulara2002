e8f91f81fca8c1d58d2ef2eb7e595b87c480349a
khushifulara2002 <106725724+khushifulara2002@users.noreply.github.com>
2022-06-09 23:07:52 +0530

    Create 87.py

---
+++ 87.py
@@ -0,0 +1,8 @@
+#program to chk lower case or not
+
+char=input("please enter any character =")
+if(char>='a'and char<='z'):
+    print("%c is a lowercase"%char)
+else:
+    print("%c is not a lowercase"%char)
+
